3967902ad251892448fa8b0bd424fc263183b477
Yoshiaki Nejime <51129480+Shimejime@users.noreply.github.com>
2021-06-24 21:40:47 +0900

    add: base folder

---
+++ base/__init__.py
@@ -0,0 +1,2 @@
+from base.ftx_api import FTXAPI
+from base.broker import Broker, Position
---
+++ base/broker.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pandas as pd
+
+from typing import List
+
+
+class Broker:
+    def __init__(self, data: pd.DataFrame, assets: float, fee: float):
+        self.data: pd.DataFrame = data
+        self.current_step = 0
+        self.assets = assets
+        self.fee = fee
+        self.position: Position = Position(self)
+
+    def buy(self, size, limit_price):
+        return self.new_order(size, limit_price)
+
+    def sell(self, size, limit_price):
+        return self.new_order(-size, limit_price)
+
+    def new_order(self, size, limit_price):
+        self.position.size = size
+        self.position.entry_price = limit_price
+
+    def get_candles(self, start, end) -> np.ndarray:
+        return self.data.iloc[start:end, :].values
+
+    @property
+    def latest_candle(self) -> np.ndarray:
+        return self.data.iloc[self.current_step, :].values
+
+    @property
+    def current_datetime(self):
+        return self.data.iloc[self.current_step, :].name
+
+    @property
+    def current_price(self) -> np.ndarray:
+        return self.data["Close"][self.current_step]
+
+    @property
+    def account_state(self) -> np.ndarray:
+        return np.array([self.assets, self.position.profit_or_loss])
+
+
+class Position:
+    def __init__(self, broker: Broker):
+        self.__broker = broker
+        self.__size = 0
+        self.__entry_price = None
+
+    def __repr__(self) -> str:
+        return f"Position(size: {self.size}, entry_price: {self.entry_price})"
+
+    @property
+    def size(self) -> float:
+        return self.__size
+
+    @property
+    def entry_price(self) -> float:
+        return self.__entry_price
+
+    @size.setter
+    def size(self, size):
+        self.__size = size
+
+    @entry_price.setter
+    def entry_price(self, price):
+        self.__entry_price = price
+
+    @property
+    def is_long(self) -> bool:
+        return True if self.__size > 0 else False
+
+    @property
+    def is_short(self) -> bool:
+        return True if self.__size < 0 else False
+
+    @property
+    def profit_or_loss(self):
+        if self.__size == 0:
+            return 0
+        return self.__size * (self.__broker.current_price - self.__entry_price)
+
+    def close(self):
+        self.__broker.assets += self.profit_or_loss
+        self.__size = 0
+        self.__entry_price = None
