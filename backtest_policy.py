3c7a59d6250b1db5788dbb7c0102335b360f61fc
Yoshiaki Nejime <51129480+Shimejime@users.noreply.github.com>
2021-07-11 03:39:55 +0900

    update: refactoring

--- backtest_policy.py
+++ backtest_policy.py
@@ -3,11 +3,10 @@ from yaml import load
 from envs.trading_env import TradingEnv
 from backtesting import Strategy, Backtest
 from ta.trend import sma_indicator
-from stable_baselines3 import A2C
+from stable_baselines3 import A2C, PPO
 
-from broker import FTXAPI
 from envs import TradingEnv
-from utils import load_data
+from utils import load_data, preprocessing
 
 np.set_printoptions(suppress=True)  # 指数表記禁止
 
@@ -15,46 +14,46 @@ np.set_printoptions(suppress=True)  # 指数表記禁止
 class DRLStrategy(Strategy):
     def init(self):
         self.model = A2C.load("a2c")
-        self.lookback_window = 200
+        # self.model = PPO.load("ppo")
+        self.lookback_window = 50
         self.state_size = 7
-        self.env = TradingEnv(lookback_window=self.lookback_window, df=self.data.df, assets=self._broker._cash)
+        self.env = TradingEnv(
+            lookback_window=self.lookback_window, df=self.data.df, preprocessed_df=preprocessing(self.data.df), assets=self._broker._cash
+        )
         self.state = self.env.reset()
         self.action = 0
         self.done = False
         self.sma = self.I(sma_indicator, self.data.Close.s, 14)
-        self.max_step = len(self.data.df)
+        self.max_step = len(self.data.df) - 1
 
     def next(self):
         self.step = len(self.data.df) - 1
         self.env.current_step = self.step  # BacktestのステップとEnvironmentのステップを同期させる
         self.env.broker.current_step = self.step
-        self.env.update_state()
 
-        if self.step < self.lookback_window:
+        if self.step < self.lookback_window or self.env.is_terminal:
             return
 
-        if self.step == self.max_step:
+        if self.step + 1 == self.max_step:
             self.env.broker.position.close()
-            self.position.close()
-            return
 
-        print("=" * 50, self.step, "=" * 50)
+        self.env.update_state()
         assert self._broker._cash, self.env.broker.assets
         assert self.equity == self.env.broker.equity
-        print(self._broker.position, self.env.broker.position)
-        print(self._broker.trades)
-        print("Assets:", self._broker._cash, self.env.broker.assets)
-        print("Equity:", self.equity, self.env.broker.equity)
-        print("Close Price:", self._broker.last_price, self.env.broker.current_price)
-        print("State: ", self.env.state[-self.state_size :])
-        print("Done", self.env.is_terminal)
+        # print("=" * 50, self.step, "=" * 50)
+        # print(self._broker.position, self.env.broker.position)
+        # print(self._broker.trades)
+        # print("Assets:", self._broker._cash, self.env.broker.assets)
+        # print("Equity:", self.equity, self.env.broker.equity)
+        # print("Close Price:", self._broker.last_price, self.env.broker.current_price)
+        # print("State: ", self.env.state[-self.state_size :])
+        # print("Done", self.env.is_terminal)
 
         self.action, _ = self.model.predict(self.env.state)
         if self.action == 0:
             pass
 
         elif self.action == 1 and not self.position.is_long:
-            print("BUY")
             if self.position.is_short:
                 self.position.close()
             else:
@@ -62,7 +61,6 @@ class DRLStrategy(Strategy):
             self.env.buy()
 
         elif self.action == 2 and not self.position.is_short:
-            print("SELL")
             if self.position.is_long:
                 self.position.close()
             else:
@@ -71,16 +69,15 @@ class DRLStrategy(Strategy):
 
 
 def main():
-    ftx = FTXAPI()
-    df = load_data("./data/ETHUSD/15", 5)
+    # ftx = FTXAPI()
     # df = ftx.fetch_candle("ETH-PERP", interval=15 * 60, limit=800)
+    df = load_data("./data/ETHUSD/15", 5)
     bt = Backtest(df, DRLStrategy, cash=10000, commission=0.0, trade_on_close=True, exclusive_orders=False)
     stats = bt.run()
-    # bt.plot(plot_volume=False)
+    bt.plot(plot_volume=False)
     print(stats)
-    print(stats["_trades"])
-    print(stats["_strategy"].env.broker.closed_trades)
-    print(df.tail(3))
+    # print(stats["_trades"])
+    # print(stats["_strategy"].env.broker.closed_trades)
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -1,16 +1,17 @@
 import numpy as np
 import pandas as pd
+import warnings
+import os
 
-from broker import FTXAPI
 from envs import TradingEnv
-from utils import load_data
+from utils import load_data, preprocessing
 
 
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3 import A2C
+from stable_baselines3 import A2C, PPO
 
 
 def evaluate(model: A2C, env, render=True):
@@ -40,18 +41,22 @@ def evaluate(model: A2C, env, render=True):
 
 
 def main():
-    ftx = FTXAPI()
+    log_dir = "./logs/"
+    os.makedirs(log_dir, exist_ok=True)
+    # ftx = FTXAPI()
     # df = ftx.fetch_candle("ETH-PERP", interval=15 * 60, limit=3 * 672)
     df = load_data("./data/ETHUSD/15", limit_days=5)
-    lookback_window = 200
-    env = TradingEnv(lookback_window=lookback_window, df=df, assets=10000)
-    # check_env(env, warn=True)
+    preprocessed_df = preprocessing(df)
+    lookback_window = 50
+    env = TradingEnv(lookback_window=lookback_window, df=df, preprocessed_df=preprocessed_df, assets=10000, fee=0.0001)
+    check_env(env, warn=True)
     env = make_vec_env(lambda: env, n_envs=1)
-    model = A2C("MlpPolicy", env, verbose=1).learn(total_timesteps=30000)
+    model = A2C("MlpPolicy", env, verbose=1, tensorboard_log=log_dir).learn(total_timesteps=30000)
     model.save("a2c")
+    # model = PPO("MlpPolicy", env, verbose=1).learn(total_timesteps=30000)
+    # model.save("ppo")
     # mean_reward, _ = evaluate_policy(model, env, render=False)
     mean_reward, std_reward = evaluate(model, env, render=True)
-    # mean_reward, std_reward = evaluate(model, env, render=False)
     print(f"mean reward: {mean_reward}, std reward: {std_reward}")
 
 
