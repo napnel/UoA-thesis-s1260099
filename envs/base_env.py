5cdd9a2ed138302e321b5d8747d71a6f8a366c36
Yoshiaki Nejime <51129480+Shimejime@users.noreply.github.com>
2021-09-03 13:26:26 +0900

    update: fixed trade size

--- envs/base_env.py
+++ envs/base_env.py
@@ -17,8 +17,8 @@ class Position:
     def __init__(self, env: gym.Env):
         self.__env = env
         self.size: int = 0
-        self.entry_price: Optional[float] = None
-        self.entry_time: Optional[float] = None
+        self.entry_price: float = 0.0
+        self.entry_time: Optional[pd.Timestamp] = None
 
     def __repr__(self) -> str:
         return f"Position(size: {self.size}, entry_price: {self.entry_price}, pl: {self.profit_or_loss:.0f})"
@@ -35,41 +35,42 @@ class Position:
     def profit_or_loss(self) -> float:
         if self.size == 0:
             return 0
-        return self.size * (self.__env.current_price - self.entry_price)
+        return self.size * (self.__env.opening_price - self.entry_price)
 
     @property
     def profit_or_loss_pct(self) -> float:
         if self.size == 0:
             return 0
-        return copysign(1, self.size) * (self.__env.current_price - self.entry_price) / self.entry_price
+        return copysign(1, self.size) * (self.__env.opening_price - self.entry_price) / self.entry_price
 
     def close(self):
         if self.size == 0:
             return
         self.__env.wallet.assets += self.profit_or_loss
         trade = {
+            "Steps": self.__env.current_step,
             "Size": self.size,
             "EntryPrice": self.entry_price,
-            "ExitPrice": self.__env.current_price,
+            "ExitPrice": self.__env.opening_price,
             "ReturnPct": self.profit_or_loss_pct,
             "EntryTime": self.entry_time,
             "ExitTime": self.__env.current_datetime,
         }
         self.__env.closed_trades = self.__env.closed_trades.append(trade, ignore_index=True)
         self.size = 0
-        self.entry_price = None
+        self.entry_price = 0.0
         self.entry_time = None
 
 
 class Wallet:
-    def __init__(self, env: gym.Env, assets: float = 1000000):
+    def __init__(self, env: gym.Env, assets: Optional[float] = None):
         self.__env = env
-        self.initial_assets = assets
-        self.assets = assets
+        self.initial_assets = 10 ** len(str(self.__env.df["High"].max()).split(".")[0]) if assets is None else assets
+        self.assets = self.initial_assets
 
     @property
     def equity(self) -> float:
-        return self.assets + self.__env.position.profit_or_loss
+        return self.assets + self.__env.position.size * (self.__env.closing_price - self.__env.position.entry_price)
 
     @property
     def equity_pct(self) -> float:
@@ -77,15 +78,22 @@ class Wallet:
 
     @property
     def free_assets(self) -> float:
-        used_assets = abs(self.__env.position.size) * self.__env.current_price
+        used_assets = abs(self.__env.position.size) * self.__env.closing_price
         return max(0, self.equity - used_assets)
 
 
 class BaseTradingEnv(gym.Env):
-    def __init__(self, df: pd.DataFrame, features: pd.DataFrame, window_size: int = 20, fee: float = 0.001, actions: Enum = Actions):
+    def __init__(
+        self,
+        df: pd.DataFrame,
+        features: pd.DataFrame,
+        window_size: int = 20,
+        fee: float = 0.001,
+        actions: Enum = Actions,
+    ):
         """ """
-        self._df = df.copy()
-        self.features = features
+        self.df = df.copy()
+        self.features = features.copy()
         self.fee = fee
         self.window_size = window_size
         self.actions = actions
@@ -99,54 +107,60 @@ class BaseTradingEnv(gym.Env):
         self.closed_trades: Optional[pd.DataFrame] = None
 
     def reset(self):
+        self.done = False
+        self.next_done = False
         self.current_step = self.window_size
         self.position = Position(self)
         self.wallet = Wallet(self)
         self.observation = self.features.iloc[: self.window_size, :].values
-        self.closed_trades = pd.DataFrame(columns=["Size", "EntryPrice", "ExitPrice", "ReturnPct", "EntryTime", "ExitTime"])
+        self.closed_trades = pd.DataFrame(columns=["Steps", "Size", "EntryPrice", "ExitPrice", "ReturnPct", "EntryTime", "ExitTime"])
         return self.observation
 
     def step(self, action):
         self.action = action
-        if self.action == Actions.Buy.value and not self.position.is_long:
-            self.buy()
+
+        # Trade Start
+        if self.next_done:
+            self.done = True
+            self.position.close()
+
+        elif self.action == Actions.Buy.value and not self.position.is_long:
+            self.buy(size=1)
 
         elif self.action == Actions.Sell.value and not self.position.is_short:
-            self.sell()
+            self.sell(size=1)
 
-        self.done = True if self.current_step + 1 == len(self._df) else False
-        if self.done:
-            self.position.close()
+        # Trade End
+
+        self.current_step += 1
+        self.next_done = True if self.current_step >= len(self.df) - 3 else False
 
         self.observation = self.next_observation
         self.reward = self._calculate_reward()
-
         self.info = {}
 
-        self.current_step += 1
         return self.observation, self.reward, self.done, self.info
 
     def render(self, mode="human"):
-        print(f"Step: {self.current_step}")
-        print(f"Assets: {self.wallet.assets}, Equity: {self.wallet.equity}")
+        print("===" * 10, f"Step: {self.current_step}", "===" * 10)
+        print(f"Equity: {self.wallet.equity}")
         print(f"Position: {self.position.size}, {self.position.profit_or_loss}")
         print(f"Action: {self.action}, Reward: {self.reward}, Done: {self.done}")
-        print(self.closed_trades.tail(1))
 
-    def buy(self):
+    def buy(self, size: Optional[float] = None):
         if self.position.size == 0:
-            adjusted_price = self.current_price * (1 + self.fee)
-            self.position.size = int(self.wallet.free_assets // adjusted_price)
+            adjusted_price = self.opening_price * (1 + self.fee)
+            self.position.size = int(self.wallet.free_assets // adjusted_price) if size is None else size
             self.position.entry_price = adjusted_price
             self.position.entry_time = self.current_datetime
 
         elif self.position.is_short:
             self.position.close()
 
-    def sell(self):
+    def sell(self, size: Optional[float] = None):
         if self.position.size == 0:
-            adjusted_price = self.current_price * (1 - self.fee)
-            self.position.size = -int(self.wallet.free_assets // adjusted_price)
+            adjusted_price = self.opening_price * (1 - self.fee)
+            self.position.size = -int(self.wallet.free_assets // adjusted_price) if size is None else -size
             self.position.entry_price = adjusted_price
             self.position.entry_time = self.current_datetime
 
@@ -158,12 +172,16 @@ class BaseTradingEnv(gym.Env):
 
     @property
     def next_observation(self):
-        return self.features[self.current_step - self.window_size + 1 : self.current_step + 1].values
+        return self.features[self.current_step - self.window_size : self.current_step].values
 
     @property
-    def current_price(self):
-        return self._df["Close"][self.current_step]
+    def closing_price(self):
+        return self.df["Close"][self.current_step]
+
+    @property
+    def opening_price(self):
+        return self.df["Open"][self.current_step + 1]
 
     @property
     def current_datetime(self):
-        return self._df.iloc[self.current_step, :].name
+        return self.df.index[self.current_step + 1]
