98aec6e433441ec18c224d2104159d0a66f915f1
Shimejime <yoshi3njm@gmail.com>
2021-10-06 12:37:49 +0900

    update: refactoring

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from src.envs.trading_env import DescTradingEnv
 from src.envs.reward_func import equity_log_return_reward
 from src.models.batch_norm import TorchBatchNormModel
 from src.utils import DataLoader, Preprocessor, backtest
-from src.utils.logger import custom_log_creator
+from src.utils.logger import custom_log_creator, create_log_filename
 from src.utils.misc import clean_result, clean_stats, send_line_notification
 
 
@@ -64,47 +64,57 @@ def main(args):
         "seed": 3407,
     }
 
+    log_filename = create_log_filename(args)
+
     ppo_config = ppo.DEFAULT_CONFIG.copy()
     ppo_config.update(config)
-    agent = ppo.PPOTrainer(config=ppo_config, logger_creator=custom_log_creator("./ray_results", args.log_file))
+    agent = ppo.PPOTrainer(config=ppo_config, logger_creator=custom_log_creator("./ray_results", log_filename))
 
     if args.checkpoint:
         agent.restore(args.checkpoint)
 
     if args.backtest:
         env_eval = DescTradingEnv(**config["evaluation_config"]["env_config"])
-        stats = backtest(env_eval, agent, plot=True, plot_filename=os.path.join(agent.logdir, "backtest"))
+        stats = backtest(env_eval, agent, plot=True, plot_filename=os.path.join(agent.logdir, f"backtest_{args.checkpoint}"))
         print(clean_stats(stats))
 
     else:
+        max_episode_reward_mean = 0
         while True:
             result = agent.train()
             print(pretty_print(clean_result(result)))
+            if result["evaluation"]["episode_reward_mean"] > max_episode_reward_mean:
+                max_episode_reward_mean = result["evaluation"]["episode_reward_mean"]
+                best_checkpoint = agent.save(f"{agent.logdir}/best")
+
             if result["timesteps_total"] >= args.stop_timesteps:
                 break
 
-        checkpoint = agent.save()
+        last_checkpoint = agent.save()
 
         env_train = DescTradingEnv(**config["env_config"])
         env_eval = DescTradingEnv(**config["evaluation_config"]["env_config"])
         backtest(env_train, agent, plot=True, plot_filename=os.path.join(agent.logdir, "backtest_train"))
-        stats = backtest(env_eval, agent, plot=True, plot_filename=os.path.join(agent.logdir, "backtest_eval"))
-        stats.to_csv(f"{agent.logdir}/stats.csv", header=False)
-        print(clean_stats(stats))
-        print("checkpoint saved at", checkpoint)
+        stats_last = backtest(env_eval, agent, plot=True, plot_filename=os.path.join(agent.logdir, "backtest_eval_last"))
+        stats_last.to_csv(f"{agent.logdir}/stats_last.csv", header=False)
+        agent.restore(best_checkpoint)
+        stats_best = backtest(env_eval, agent, plot=True, plot_filename=os.path.join(agent.logdir, "backtest_eval_best"))
+        stats_best.to_csv(f"{agent.logdir}/stats_best.csv", header=False)
+        print(clean_stats(stats_last))
+        print("last checkpoint saved at", last_checkpoint)
+        print("best checkpoint saved at", best_checkpoint)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--backtest", action="store_true")
     parser.add_argument("--checkpoint", type=str, default=None)
-    parser.add_argument("--ticker", type=str, default="^N225")
     parser.add_argument("--window_size", type=int, default=5)
     parser.add_argument("--fee", type=float, default=0.001)
     parser.add_argument("--model", type=str, default=None)
-    parser.add_argument("--hiddens", nargs="*", default=[128, 128])
-    parser.add_argument("--stop-timesteps", type=int, default=1e6)
-    parser.add_argument("--log-file", type=str, default="PPO")
+    parser.add_argument("--hiddens", type=int, nargs=2, default=[256, 256])
+    parser.add_argument("--ticker", type=str, default="^N225")
+    parser.add_argument("--stop-timesteps", type=int, default=int(1e6))
 
     args = parser.parse_args()
     print(f"Running with following CLI options: {args}")
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,14 +1,31 @@
 import os
+from argparse import Namespace
 import tempfile
 from datetime import datetime
 
 from ray.tune.logger import UnifiedLogger
 
 
-def custom_log_creator(custom_path, custom_str):
+def create_log_filename(args: Namespace):
+    filename = ""
+
+    omited_list = ("backtest", "stop_timesteps", "checkpoint")
+
+    for key, value in args.__dict__.items():
+        if value and key not in omited_list:
+            if isinstance(value, list):
+                value = tuple(value)
+            filename += f"({key}={value})_"
+
+    filename = filename.replace(" ", "")
+    filename = filename[:-1]
+    return filename
+
+
+def custom_log_creator(custom_path: str, custom_str: str = ""):
     custom_path = os.path.expanduser(custom_path)
 
-    timestr = datetime.today().strftime("%Y-%m-%d_%H-%M-%S")
+    timestr = datetime.today().strftime("%m-%d_%H-%M")
     logdir_prefix = f"{custom_str}_{timestr}"
 
     def logger_creator(config):
@@ -16,7 +33,7 @@ def custom_log_creator(custom_path, custom_str):
         if not os.path.exists(custom_path):
             os.makedirs(custom_path)
         logdir = tempfile.mkdtemp(prefix=logdir_prefix, dir=custom_path)
+        # logdir = os.path.join(custom_path, logdir_prefix)
         return UnifiedLogger(config, logdir, loggers=None)
 
     return logger_creator
-
